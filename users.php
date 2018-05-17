82902c0d50fbeeb0dc1046527f574d8ed24f35cf
Ľubica Dúbravcová <32597067+LubicaDubravcova@users.noreply.github.com>
2018-05-17 22:36:53 +0200

    doplnene

--- users.php
+++ users.php
@@ -2,57 +2,60 @@
 header('Content-Type: text/html; charset=utf-8');
 include_once("workers/dbConn.php");
 $db = new DBConn();
-$database = $db->getDB();
-$sql = "SELECT users.ID, users.FIRSTNAME, users.SURNAME, addresses.ADDRESS, addresses.CITY, addresses.PSC, schools.NAME  
-FROM users JOIN addresses ON users.ADDRESS_ID=addresses.ID
-JOIN schools ON users.SCHOOL_ID=schools.ID";
-$result = $database->query($sql);
+$sql = "SELECT users.ID, CONCAT(users.FIRSTNAME, ' ', users.SURNAME) as NAME, CONCAT(a.ADDRESS, ', ', a.CITY) as ADDRESS, CONCAT(schools.NAME, ', ' , sa.CITY) as SCHOOL
+FROM users JOIN addresses a ON users.ADDRESS_ID=a.ID JOIN schools ON users.SCHOOL_ID=schools.ID JOIN addresses sa ON sa.ID=schools.ADDRESS_ID";
+$assoc = $db->getAssoc($sql);
 ?>
 <!doctype html>
 <html>
 <head>
     <title>Záverečné Zadanie</title>
     <?php require("includes/head.php");?>
+	<style>.clickable-row{cursor: pointer;}</style>
 </head>
 <body class="bg-dark text-white">
 <?php require("includes/navbar.php"); ?>
 <div class="container text-center">
     <div class="row">
         <div class="col">
-            <h2 class="m-4 d-inline-block">Zoznam používateľov</h2>
+            <h2 class="m-4 d-inline-block">Zoznam užívateľov</h2>
         </div>
     </div>
     <div class="row justify-content-center bg-light text-dark rounded p-5">
         <div class="col">
-            <div class='table-responsive'>
-                <table class='table'>
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Užívateľ</th>
-                        <th>Adresa</th>
-                        <th>Škola</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach($result->fetch_all(MYSQLI_ASSOC) as $user): ?>
-                        <tr>
-                            <td><?php echo $user["ID"]; ?></td>
-                            <td>
-                                <a href='index.php?open=<?php echo $user["ID"]; ?>'>
-                                    <?php echo $user["FIRSTNAME"]." ".$user["SURNAME"]; ?>
-                                </a>
-                            </td>
-                            <td><?php echo $user["ADDRESS"].", ".$user["CITY"]." ".$user["PSC"]; ?></td>
-                            <td><?php echo $user["NAME"]; ?></td>
-                        </tr>
-                    <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
+			<table class='table table-hover'>
+				<thead>
+				<tr>
+					<th>#</th>
+					<th>Užívateľ</th>
+					<th>Adresa</th>
+					<th>Škola</th>
+				</tr>
+				</thead>
+				<tbody>
+				<?php foreach($assoc as $user): ?>
+					<tr class='clickable-row' data-href='index.php?open=<?php echo $user["ID"]; ?>'>
+						<td><?php echo $user["ID"]; ?></td>
+						<td><?php echo $user["NAME"]; ?></td>
+						<td><?php echo $user["ADDRESS"]; ?></td>
+						<td><?php echo $user["SCHOOL"]; ?></td>
+					</tr>
+				<?php endforeach; ?>
+				</tbody>
+			</table>
+            <form method="post" action="#" class="form-inline">
+				<label for="file" class="form-control-plaintext">Načítanie CSV s užívateľmi: </label>
+				<input type="file" class="form-control-file" name="file" id="file" accept=".csv" required/>
+				<input type="submit" class="form-control col-4"/>
+			</form>
         </div>
     </div>
 </div>
+<script>
+$(".clickable-row").click(function() {
+	window.location = $(this).data("href");
+});
+</script>
 <?php require("includes/footer.php");?>
 </body>
 </html>
