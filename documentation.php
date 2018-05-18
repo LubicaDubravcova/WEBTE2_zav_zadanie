52922665ca3c8de335a9608ea96fc2d72782f285
Ľubica Dúbravcová <32597067+LubicaDubravcova@users.noreply.github.com>
2018-05-18 18:18:55 +0200

    "hruba" dokumentacia

---
+++ documentation.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html>
+<head>
+    <title>Záverečné Zadanie</title>
+    <?php require("includes/head.php");?>
+</head>
+<body class="bg-dark text-white">
+<?php require("includes/navbar.php"); ?>
+<div class="container text-center">
+    <div class="row">
+        <div class="col">
+            <h2 class="m-4 d-inline-block">Technická dokumentácia</h2>
+        </div>
+    </div>
+    <div class="row justify-content-center bg-light text-dark rounded p-5">
+        <div class="col">
+            <table class='table sortable'>
+                <thead>
+                <tr>
+                    <th>Patrik Kadlčík</th>
+                    <th>Jakub Kovář</th>
+                    <th>Jakub Kováčik</th>
+                    <th>Nikola Zarembová</th>
+                    <th>Ľubica Dúbravcová</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>
+                        <ul class="list-group">
+                            <li class="list-group-item">Databáza</li>
+                            <li class="list-group-item">Dizajn</li>
+                            <li class="list-group-item">Registrácia + prihlásenie</li>
+                            <li class="list-group-item">Titulná stránka dostupná bez prihlásenia</li>
+                            <li class="list-group-item">Okamžitá aktualizácia po pridaní nových údajov</li>
+                            <li class="list-group-item">Dolaďovanie</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul class="list-group">
+                            <li class="list-group-item">Návrh webovej aplikácie</li>
+                            <li class="list-group-item">Grafické odlíšenie trás družstiev/jedincov</li>
+                            <li class="list-group-item">Mód tréningu</li>
+                            <li class="list-group-item">Nová trasa</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul class="list-group">
+                            <li class="list-group-item">Tabuľka pre každého užívateľa (jeho výkony)</li>
+                            <li class="list-group-item">Štafetový mód (družstvo a jeho modifikácia) </li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul class="list-group">
+                            <li class="list-group-item">Zobrazenie aktualít zadávaných administrátorom </li>
+                            <li class="list-group-item">Generovanie tabuľky užívateľa do pdf súboru</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul class="list-group">
+                            <li class="list-group-item">Tabuľka všetkých užívateľov pre administrátora</li>
+                            <li class="list-group-item">Tabuľka trás</li>
+                            <li class="list-group-item">Dokumentácia</li>
+                        </ul>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
+    <div class="row justify-content-center bg-dark text-light rounded p-5">
+        <div class="container">
+            <p> Bližšiu technickú dokumentáciu prikladáme ako .docx súbor k zdrojovým súborom. <br>
+                Zverejnením detailov na samotnej stránke by sme k jej zabezpečeniu veľmi neprispeli. </p>
+        </div>
+    </div>
+
+</div>
+<?php require("includes/footer.php");?>
+</body>
+</html>
+
