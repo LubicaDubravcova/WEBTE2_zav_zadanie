159e441a470b1adb1a451043cb7ee51f0ed94764
minop <jakub@kovar.co.uk>
2018-05-18 11:06:29 +0200

    Bugfix zobrazovanie uzivatela v tyme len 1 (nie podla poctu treningov) a zoradenie clenov tymu podla priezviska

--- workers/dbConn.php
+++ workers/dbConn.php
@@ -489,7 +489,7 @@ class DBConn {
 	// zaznamy su usporiadane podla LENGTH zostupne (descending)
 	// POZOR! pole je indexovane netypicky! pole[STLPEC][RIADOk] (viac mi to tak vyhovuje pri praci s nim)
 	function getRelayRouteProgress($routeId) {
-		$stmt = $this->db->prepare("SELECT users_teams.TEAM_ID AS TID, GROUP_CONCAT(CONCAT(users.FIRSTNAME,\" \",users.SURNAME) SEPARATOR \", \") AS MEMBERS, COALESCE(SUM(trainings.`LENGTH`), 0) AS LENGTH FROM users INNER JOIN users_teams ON users_teams.USER_ID = users.ID LEFT JOIN trainings ON trainings.`USER_ID` = users.ID WHERE trainings.`ROUTE_ID` = ? OR `trainings`.`USER_ID` IS NULL GROUP BY users_teams.TEAM_ID ORDER BY LENGTH DESC");
+		$stmt = $this->db->prepare("SELECT team_progress.TID, MEMBERS, LENGTH FROM (SELECT users_teams.TEAM_ID AS TID, COALESCE(SUM(trainings.`LENGTH`), 0) AS LENGTH FROM users INNER JOIN users_teams ON users_teams.USER_ID = users.ID LEFT JOIN trainings ON trainings.`USER_ID` = users.ID WHERE trainings.`ROUTE_ID` = ? OR `trainings`.`USER_ID` IS NULL GROUP BY users_teams.TEAM_ID) AS team_progress JOIN (SELECT GROUP_CONCAT(CONCAT(users.FIRSTNAME, \" \", users.SURNAME) ORDER BY users.SURNAME SEPARATOR \", \") AS MEMBERS, users_teams.TEAM_ID AS TID FROM users_teams JOIN users ON users_teams.USER_ID = users.ID GROUP BY users_teams.TEAM_ID) as team_names ON team_names.TID = team_progress.TID  ORDER BY LENGTH DESC");
 
 		if ($stmt === false) {
 			trigger_error($this->db->error, E_USER_ERROR);
