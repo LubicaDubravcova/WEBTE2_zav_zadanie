2c4d4edd422a893fe8d4cd38cfe8b6bdcdba39f6
minop <jakub@kovar.co.uk>
2018-05-19 22:34:11 +0200

    pridanie novych farieb do palety

--- workers/routeColorPalette.php
+++ workers/routeColorPalette.php
@@ -6,11 +6,15 @@ class routeColorPalette
 	// farby progressov
 	public static $subrouteColors = [
 		"#FF0000",
+		"#FF7F00",
 		"#FFFF00",
+		"#7FFF00",
 		"#00FF00",
+		"#00FF7F",
 		"#00FFFF",
+		"#007FFF",
 		"#0000FF",
-		"#FF00FF"
+		"#7F00FF"
 	];
 
 	// prida farby ako javascript premenne
